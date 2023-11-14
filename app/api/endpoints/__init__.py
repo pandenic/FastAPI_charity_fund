8eb569536d4158fe6dfb9e29ad83d8c926fa71ac
Denis <panasenkodf@gmail.com>
2023-11-14 10:26:04 +0300

    PEP8. Docstrings

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,5 +1,6 @@
 """Contain all imports from modules."""
-from app.api.endpoints.charity_project import \
-    router as charityproject_router  # noqa
+from app.api.endpoints.charity_project import (
+    router as charityproject_router
+) # noqa
 from app.api.endpoints.donation import router as donation_router  # noqa
 from app.api.endpoints.user import router as user_router  # noqa
